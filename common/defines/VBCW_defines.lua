9991e3a01c26fa9cf1e2023907a7fe331c7a614c
SquireBev <40180921+SquireBev@users.noreply.github.com>
2018-11-15 20:09:40 +0000

    Factory Output Rebalance - Base

--- common/defines/VBCW_defines.lua
+++ common/defines/VBCW_defines.lua
@@ -1,4 +1,7 @@
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 0.5
+NDefines.NProduction.BASE_FACTORY_SPEED = 2
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2
+NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 1
 NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 50
 NDefines.NBuildings.RADAR_RANGE_MIN = 150
 NDefines.NBuildings.RADAR_RANGE_MAX = 1500
@@ -8,18 +11,11 @@ NDefines.NAir.AIR_WING_MAX_SIZE = 50
 NDefines.NAir.ACE_WING_SIZE = 12
 NDefines.NNavy.NAVAL_SPEED_MODIFIER = 1.5
 NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 1.8
+
+
 NDefines_Graphics.NGraphics.VICTORY_POINT_LEVELS = 2
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20}
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {150, 450, 1200}
 NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {300, 500, 1500}
 NDefines_Graphics.NGraphics.GMT_OFFSET = 3375
-NDefines_Graphics.NFrontend.CAMERA_LOOKAT_Z = 400
-NDefines_Graphics.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 400
-NDefines_Graphics.NFrontend.CAMERA_START_Z = 400
-NDefines_Graphics.NFrontend.CAMERA_END_Z = 400
-NDefines_Graphics.NFrontend.FRONTEND_POS_X = 3600
 NDefines_Graphics.NFrontend.FRONTEND_POS_Y = 3000
-NDefines_Graphics.NFrontend.FRONTEND_POS_Z = 1024
-NDefines_Graphics.NFrontend.FRONTEND_LOOK_X = 3600
-NDefines_Graphics.NFrontend.FRONTEND_LOOK_Y = 0
-NDefines_Graphics.NFrontend.FRONTEND_LOOK_Z = 1024
